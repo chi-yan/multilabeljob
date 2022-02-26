0cad6f89b2ab12a0d3cd37ace5b89aaca9e07c4f
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2022-02-26 16:46:00 +1100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ classifier = load_classifier()
 d = {0: 'Accounting', 1: 'Administration & Office Support', 2: 'Advertising, Arts & Media', 3: 'Banking & Financial Services', 4: 'CEO & General Management', 5: 'Call Centre & Customer Service', 6: 'Community Services & Development', 7: 'Construction', 8: 'Consulting & Strategy', 9: 'Design & Architecture', 10: 'Education & Training', 11: 'Engineering', 12: 'Farming, Animals & Conservation', 13: 'Government & Defence', 14: 'Healthcare & Medical', 15: 'Hospitality & Tourism', 16: 'Human Resources & Recruitment', 17: 'Information & Communication Technology', 18: 'Insurance & Superannuation', 19: 'Legal', 20: 'Manufacturing, Transport & Logistics', 21: 'Marketing & Communications', 22: 'Mining, Resources & Energy', 23: 'Real Estate & Property', 24: 'Retail & Consumer Products', 25: 'Sales', 26: 'Science & Technology', 27: 'Self Employment', 28: 'Sport & Recreation', 29: 'Trades & Services'}
 categories = list(d.values())
     
-st.markdown(f"""<h1>Job Category Predictor</h1><br>A famous job website has 30 categories: {categories}""", unsafe_allow_html=True)
+st.markdown(f"""<h1>Job Category Predictor</h1><br>A famous job website has 30 categories: <br>{categories}<br>""", unsafe_allow_html=True)
 sentence = st.text_input('Input job description here: ')
 
 if sentence:
